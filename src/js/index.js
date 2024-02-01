9ce6c917bd5724ea8007f1994e792428019f5628
taguchi kazuki <elfubolfelicidadsuprema02@gmail.com>
2024-02-02 03:00:29 +0900

    feat:delete action ok

--- src/js/index.js
+++ src/js/index.js
@@ -4,40 +4,58 @@
     single quotes
 */
 
-const submit = document.getElementById('submit');
 const taskCount = document.getElementById('task-count');
 const taskNotStartCount = document.getElementById('task-not-start-count');
 const taskCompCount = document.getElementById('task-comp-count');
-const taskCheckBox = document.getElementById('checkbox');
 let taskContent = document.getElementById('card-item-content')
-let counter = {
-  all: 0,
-  notStart: 0,
-  comp: 0,
+let todoList = [];  // status: 0: 未着手, 1: 完了
+
+const getCountByStatus = (status) => {
+  const newArray = todoList.filter((todo) => {
+    return todo.status === status;
+  })
+  return newArray.length;
 }
 
-const addTask = (event) => {
-  event.preventDefault();
+const updateStatusById = (id, status) => {
+  return todoList.map((todo) => {
+    if (todo.id === id) {
+      todo.status = status;
+    }
+    return todo;
+  })
+}
+
+const filterById = (id) => {
+  return todoList.filter((todo) => {
+    return todo.id !== id;
+  })
+}
+
+const addTask = () => {
   const inputValue = document.getElementById('input-todo');
   const task = inputValue.value;
   if (task) {
-    todoList.push(task);
+    const newTask = {
+      id: todoList.length,
+      content: task,
+      status: 0,
+    }
+    todoList.push(newTask);
 
     // count fix
-    counter.all += 1;
-    counter.notStart += 1;
-    taskCount.textContent = counter.all;
-    taskNotStartCount.textContent = counter.notStart;
+    taskCount.textContent = todoList.length;
+    taskNotStartCount.textContent = getCountByStatus(0);
 
     // task view fix
     let newTaskContent = document.createElement('li');
     newTaskContent.setAttribute('class', 'card-item-wrapper');
-    newTaskContent.setAttribute('id', `card-item-index-${todoList.length - 1}`);
+    newTaskContent.setAttribute('id', `${newTask.id}`);
     newTaskContent.innerHTML = `
     <input id="checkbox" type="checkbox" class="card-item-checkbox" name="task-checkbox">
     <p class="card-item-p">${task}</p>
     <div class="card-item-btns">
-      <button class="edit">編集</button><button class="del">削除</button>
+      <button class="edit" id="edit">編集</button><button class="del" id="del">削除</button>
     </div>
     `;
     taskContent.appendChild(newTaskContent);
@@ -47,37 +65,65 @@ const addTask = (event) => {
   }
 }
 
-const changeCheckBox = (event) => {
-  const target = event.target;
-  if (target.matches('.card-item-checkbox')) {
-    // チェックボックスがクリックされた場合の処理
-    const parentElement = target.parentElement;
-    const childElement = parentElement.children[1];
-    const className = childElement.className;
-    let disabled;
-    if (target.checked) {
-      counter.notStart -= 1;
-      counter.comp += 1;
-      childElement.classList.add('cancellation');
-      disabled = true;
-    } else {
-      counter.notStart += 1;
-      counter.comp -= 1;
-      childElement.classList.remove('cancellation');
-      disabled = false;
-    }
-
-    taskNotStartCount.textContent = counter.notStart;
-    taskCompCount.textContent = counter.comp;
-
-    const childEditBtn = parentElement.children[2].children[0];
-    childEditBtn.disabled = disabled;
+// チェックボックスがクリックされた場合の処理
+const changeCheckBox = (target) => {
+  const parentElement = target.parentElement;
+  const id = Number(parentElement.id);
+  const childElement = parentElement.children[1];
+  let disabled;
+  let status;
+  if (target.checked) {
+    childElement.classList.add('cancellation');
+    disabled = true;
+    status = 1;
+  } else {
+    childElement.classList.remove('cancellation');
+    disabled = false;
+    status = 0;
   }
+
+  todoList = updateStatusById(id, status);
+  taskNotStartCount.textContent = getCountByStatus(0);
+  taskCompCount.textContent = getCountByStatus(1);
+
+  const childEditBtn = parentElement.children[2].children[0];
+  childEditBtn.disabled = disabled;
 }
 
+const editTask = (target) => {
+  console.log("edit")
+  console.log(target)
+}
 
-let todoList = [];
-submit.addEventListener('click', addTask);
-document.addEventListener('change', changeCheckBox);
+const deleteTask = (target) => {
+  const parentElement = target.parentElement.parentElement;
+  const id = Number(parentElement.id);
+  parentElement.remove();
 
+  todoList = filterById(id);
+  taskCount.textContent = todoList.length;
+  taskNotStartCount.textContent = getCountByStatus(0);
+  taskCompCount.textContent = getCountByStatus(1);
+}
+
+document.addEventListener('click', (event) => {
+  // checkboxのイベントさえも無効化してしまう、どうしようかな
+  // event.preventDefault();
+  const target = event.target;
+
+  if (target.matches('.card-item-checkbox')) {
+    changeCheckBox(target)
+  }
+
+  if (target.matches('#submit')) {
+    addTask()
+  }
+
+  if (target.matches('#edit')) {
+    editTask(target)
+  }
+  if (target.matches('#del')) {
+    deleteTask(target)
+  }
+})
 
